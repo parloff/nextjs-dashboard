2ffc5448a0aaee1f4fdc10530c761679095727d3
Adekunle Victor <134967832+parloff@users.noreply.github.com>
2025-06-04 06:35:17 +0100

    Replace Lusitana font with Poppins in various components for consistent typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import '@/app/ui/global.css';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='{`${inter.className} antialiased`}'  >{children}</body>
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
